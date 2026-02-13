4fd156e9faa7e17aee2dec4b066eb353c699e9fa
Paulo Guerreiro <paulocmguerreiro@gmail.com>
2026-02-13 15:44:24 +0000

    Refactoring: Restruturação e organização do processo de mutação no lado do Exemplo - HorárioEscolar

--- Exemplos/HorarioEscolar/Individuo/Extension/MutacaoService.cs
+++ Exemplos/HorarioEscolar/Individuo/Extension/MutacaoService.cs
@@ -2,6 +2,8 @@ using AlgoritmoGenetico.Configuracao;
 using AlgoritmoGenetico.Individuo;
 using HorarioEscolar.Helper;
 using HorarioEscolar.Factories;
+using System.Runtime.CompilerServices;
+using AlgoritmoGenetico.Mutacao;
 
 namespace HorarioEscolar.Individuo.Extension
 {
@@ -9,11 +11,8 @@ namespace HorarioEscolar.Individuo.Extension
     /// Fornece métodos de extensão e lógica especializada para aplicar mutações em cromossomas de horários.
     /// Esta classe gere a alteração cirúrgica de salas, dias e horas, respeitando as restrições de domínio.
     /// </summary>
-    public class MutacaoExtensions<TCromossoma> where TCromossoma : ICromossoma<IGene>
+    public class MutacaoService(IHorarioService horarioService, IDisciplinaService disciplinaService) : Multipla<HorarioCromossoma>
     {
-        /// <summary> Referência para as definições globais do motor (taxas de mutação, limites, etc.). </summary>
-        private AGConfiguracao<TCromossoma>? _configuracao;
-
         /// <summary>
         /// Tenta aplicar sequencialmente mutação de Sala, Dia ou Hora até que uma seja bem-sucedida.
         /// </summary>
@@ -22,29 +21,23 @@ namespace HorarioEscolar.Individuo.Extension
         private bool TryAplicarMutacao(ref HorarioGene gene) =>
             TryAplicarMutacaoSala(ref gene) || TryAplicarMutacaoDia(ref gene) || TryAplicarMutacaoHora(ref gene);
 
-
         /// <summary>
         /// Ponto de entrada principal para o processo de mutação de um indivíduo.
         /// Escolhe um gene aleatoriamente e tenta aplicar uma das três estratégias de mutação disponíveis.
         /// </summary>
         /// <param name="individuoAProcessar">O cromossoma que poderá sofrer a mutação.</param>
         /// <param name="Configuracao">A configuração atual do Algoritmo Genético.</param>
-        public void ProcessarMutacao(ICromossoma<IGene> individuoAProcessar, AGConfiguracao<TCromossoma> Configuracao)
+        public override void ProcessarMutacao(HorarioCromossoma individuoAProcessar)
         {
-            _configuracao = Configuracao;
-            if (Configuracao is null)
-            {
-                return;
-            }
-            HorarioCromossoma individuo = (HorarioCromossoma)individuoAProcessar;
-            IList<HorarioGene> individuoGenes = (IList<HorarioGene>)individuo.Genes;
+            HorarioCromossoma? individuo = individuoAProcessar as HorarioCromossoma;
+            IList<HorarioGene> individuoGenes = (IList<HorarioGene>)individuo!.Genes;
 
             int geneIndex = Random.Shared.Next(individuoGenes.Count);
             HorarioGene gene = individuoGenes[geneIndex];
 
-            if (_configuracao.ProcessoDeMutacao.PodeMutar(gene) && TryAplicarMutacao(ref gene))
+            if (PodeMutar(gene) && TryAplicarMutacao(ref gene))
             {
-                individuo.ResetFitness();
+                individuo.Reset();
                 individuoGenes[geneIndex] = gene;
             }
         }
@@ -55,11 +48,6 @@ namespace HorarioEscolar.Individuo.Extension
         /// </summary>
         private bool TryAplicarMutacaoSala(ref HorarioGene gene)
         {
-            if (!_configuracao!.ProcessoDeMutacao.PodeMutar(gene))
-            {
-                return false;
-            }
-
             int totalAulas = gene.Aulas.Count;
             HorarioDiasGene[] listaTemporaria = new HorarioDiasGene[totalAulas];
 
@@ -69,7 +57,7 @@ namespace HorarioEscolar.Individuo.Extension
             {
                 return false;
             }
-            string novaSala = DisciplinaHelper.ObterSalaAleatoriaDaDisciplina(gene.Disciplina);
+            string novaSala = disciplinaService.ObterSalaAleatoria(gene.Disciplina);
             for (int aulaIndex = 0; aulaIndex < totalAulas; aulaIndex++)
             {
                 HorarioDiasGene aula = gene.Aulas[aulaIndex];
@@ -97,10 +85,6 @@ namespace HorarioEscolar.Individuo.Extension
         /// </summary>
         private bool TryAplicarMutacaoHora(ref HorarioGene gene)
         {
-            if (!_configuracao!.ProcessoDeMutacao.PodeMutar(gene))
-            {
-                return false;
-            }
 
             List<HorarioDiasGene> diasUtilizados = gene.Aulas;
             int totalAulasDias = diasUtilizados.Count;
@@ -108,7 +92,7 @@ namespace HorarioEscolar.Individuo.Extension
             int diaAlterarIndex = Random.Shared.Next(totalAulasDias);
             int diaAlterar = diasUtilizados[Random.Shared.Next(totalAulasDias)].DiaDaAula;
 
-            string horaInicial = HorarioHelper.ObterHoraAleatoriaDoDia(diasUtilizados[diaAlterarIndex].DuracaoTemposLetivos);
+            string horaInicial = horarioService.ObterHoraAleatoriaDoDia(diasUtilizados[diaAlterarIndex].DuracaoTemposLetivos);
             for (int aulaIndex = 0; aulaIndex < totalAulasDias; aulaIndex++)
             {
                 HorarioDiasGene aula = gene.Aulas[aulaIndex];
@@ -120,7 +104,7 @@ namespace HorarioEscolar.Individuo.Extension
                         aula.SalaDeAula,
                         horaInicial
                     );
-                    horaInicial = HorarioHelper.ObterHoraSeguinte(horaInicial);
+                    horaInicial = horarioService.ObterHoraSeguinte(horaInicial);
                 }
                 else
                 {
@@ -138,19 +122,15 @@ namespace HorarioEscolar.Individuo.Extension
         /// </summary>
         private bool TryAplicarMutacaoDia(ref HorarioGene gene)
         {
-            if (!_configuracao!.ProcessoDeMutacao.PodeMutar(gene))
-            {
-                return false;
-            }
             HashSet<int> diasUtilizadosDiasBuffer = new HashSet<int>(7);
             List<HorarioDiasGene> diasUtilizados = gene.Aulas;
             int totalAulasDias = diasUtilizados.Count;
             HorarioDiasGene[] listaTemporaria = new HorarioDiasGene[totalAulasDias];
             foreach (HorarioDiasGene aula in diasUtilizados)
             {
-                diasUtilizadosDiasBuffer.Add(aula.DiaDaAula); // Preenche o HashSet O(1)
+                diasUtilizadosDiasBuffer.Add(aula.DiaDaAula);
             }
-            List<int> diasDisponiveis = HorarioHelper.DiasDaSemanaIndex.Where(x => !diasUtilizadosDiasBuffer.Contains(x)).ToList();
+            List<int> diasDisponiveis = horarioService.DiasDaSemanaIndex.Where(x => !diasUtilizadosDiasBuffer.Contains(x)).ToList();
 
             int alterarParaDia = diasDisponiveis[Random.Shared.Next(diasDisponiveis.Count)];
             int alterarDoDia = diasUtilizados[Random.Shared.Next(diasUtilizados.Count)].DiaDaAula;

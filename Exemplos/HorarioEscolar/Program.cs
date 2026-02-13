05188f828ff09302cedd96b9cc701c650413463b
Paulo Guerreiro <paulocmguerreiro@gmail.com>
2026-02-13 15:48:48 +0000

    Refactoring: Restruturação e organização do programa no lado Exemplo - HorárioEscolar

--- Exemplos/HorarioEscolar/Program.cs
+++ Exemplos/HorarioEscolar/Program.cs
@@ -1,5 +1,4 @@
-﻿using AlgoritmoGenetico;
-using AlgoritmoGenetico.Recombinacao;
+﻿using AlgoritmoGenetico.Recombinacao;
 using AlgoritmoGenetico.Selecao;
 using AlgoritmoGenetico.Configuracao;
 using HorarioEscolar;
@@ -7,87 +6,89 @@ using HorarioEscolar.Helper;
 using HorarioEscolar.Individuo.Extension;
 using HorarioEscolar.Individuo;
 using AlgoritmoGenetico.Mutacao;
-using HorarioEscolar.Factories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AlgoritmoGenetico.Individuo;
+using AlgoritmoGenetico.Extensao;
+using Spectre.Console;
 
+var builder = Host.CreateApplicationBuilder(args);
 
-Console.WriteLine("Obter estrutura dos horários...");
-if (!HorarioHelper.TryCarregarHorario())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro dos Horários");
-    return;
-}
-if (!DisciplinaHelper.TryCarregarDisciplinas())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro das Disciplinas");
-    return;
-}
-if (!DisciplinaHelper.TryCarregarSalas())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro das Salas das Disciplinas");
-    return;
-}
-if (!TurmaHelper.TryCarregarTurmas())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro das Turmas");
-    return;
-}
-if (!TurmaHelper.TryCarregarTurmaHorario())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro dos Tempos Letivos dos Horários");
-    return;
-}
-if (!TurmaHelper.TryCarregarTurmaProfessores())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro dos Professores das Disciplinas da Turma");
-    return;
-}
-if (!ProfessorHelper.TryCarregarProfessores())
-{
-    Console.WriteLine("Não foi possível importar o ficheiro dos Professores");
-    return;
-}
+// DI 
+builder.Services.AddCSV();
+builder.Services.AddAlgoritmo();
 
+// SERVIÇOS
+using IHost host = builder.Build();
+var horarioService = host.Services.GetRequiredService<IHorarioService>();
+var disciplinaService = host.Services.GetRequiredService<IDisciplinaService>();
+var professoresService = host.Services.GetRequiredService<IProfessorService>();
+var turmaService = host.Services.GetRequiredService<ITurmaService>();
+var cromossomaFactory = host.Services.GetRequiredService<ICromossomaFactory<HorarioCromossoma>>();
+var mutacaoService = host.Services.GetRequiredService<IMutacaoService<HorarioCromossoma>>();
+var recombinacaoService = host.Services.GetRequiredService<IRecombinacao<HorarioCromossoma>>();
+var fitnessService = host.Services.GetRequiredService<ICromossomaFitnessService<HorarioCromossoma>>();
+var outputService = host.Services.GetRequiredService<IAGOutputService<HorarioCromossoma>>();
 
-MutacaoExtensions<HorarioCromossoma> mutacaoExt = new MutacaoExtensions<HorarioCromossoma>();
+CarregarDadosCSV();
 
-AGConfiguracao<HorarioCromossoma> agConfig = new AGConfiguracao<HorarioCromossoma>
-{
-    DimensaoDaPopulacao = 50,
-    LimiteMáximoDeGeracoesPermitidas = 50000,
-    FitnessPretendido = 0f,
-    ReporSolucaoCandidataNaPopulacaoACadaGeracao = 50,
-    DarFeedbackACadaSegundo = 1,
-    FeedbackCallback = DashboardHelper.MostrarInformacao,
-    ProcessoDeEvolucao = AGProcessoDeEvolucao.MINIMIZACAO,
+var algoritmo = ConfigurarAlgoritmo();
 
-    //ProcessoDeRecombinacao = new SemRecombinacao<HorarioCromossoma>(),
-    ProcessoDeRecombinacao = new Uniforme<HorarioCromossoma>(),
-    //ProcessoDeRecombinacao = new SinglePoint<HorarioCromossoma>(),
-    //ProcessoDeRecombinacao = new TwoPoints<HorarioCromossoma>(),
-    //ProcessoDeRecombinacao = new CycleCrossOver<HorarioCromossoma>(),
-
-    //ProcessoDeSelecaoDaProximaGeracao = new Todos<HorarioCromossoma>(),
-    //ProcessoDeSelecaoDaProximaGeracao = new Truncation<HorarioCromossoma>(),
-    ProcessoDeSelecaoDaProximaGeracao = new Crowding<HorarioCromossoma>(),
-    //ProcessoDeSelecaoDaProximaGeracao = new Tournament<HorarioCromossoma>(),
-    //ProcessoDeSelecaoDaProximaGeracao = new RouletteWheel<HorarioCromossoma>(),
-    ProbabilidadeDeSelecionarDaGeracaoPais = 0.10f,
-    ProbabilidadeDeSelecionarDaGeracaoFilhos = .90f,
-
-    //ProcessoDeMutacao = new SemMutacao<HorarioCromossoma>(),
-    //ProcessoDeMutacao = new Unica<HorarioCromossoma> { FatorMutacaoColisao = .2d, FatorMutacaoNormal = 0.1d, AjustarMutacaoACadaGeracao = 100 },
-    ProcessoDeMutacao = new Multipla<HorarioCromossoma> { FatorMutacaoColisao = .50d, FatorMutacaoNormal = 0.05d, QuantidadeDeMutacoes = 10, AjustarMutacaoACadaGeracao = 100 },
-    ProcessarMutacaoCallback = mutacaoExt.ProcessarMutacao
-
-};
-
-
-AGHorarioEscolar<HorarioCromossoma> ag = new AGHorarioEscolar<HorarioCromossoma>
-{
-    Configuracao = agConfig,
-};
-
-ag.ProcurarSolucao();
-ag.FornecerFeedback();
+algoritmo.ProcurarSolucao();
+algoritmo.FornecerFeedback();
 
 return;
+AGHorarioEscolar<HorarioCromossoma> ConfigurarAlgoritmo()
+{
+
+    AGConfiguracao<HorarioCromossoma> agConfig = new AGConfiguracao<HorarioCromossoma>
+    {
+        DimensaoDaPopulacao = 500,
+        LimiteMáximoDeGeracoesPermitidas = 50000,
+        FitnessPretendido = 0f,
+        ReporSolucaoCandidataNaPopulacaoACadaGeracao = 50,
+        DarFeedbackACadaSegundo = 1,
+        ProcessoDeEvolucao = AGProcessoDeEvolucao.MINIMIZACAO,
+        ProcessoCalculoFitness = fitnessService,
+        ProcessoDeSelecaoDaProximaGeracao = new Crowding<HorarioCromossoma>(),
+        ProcessoDeRecombinacao = recombinacaoService,
+        ProcessoDeMutacao = mutacaoService,
+        ProbabilidadeDeSelecionarDaGeracaoPais = .15f,
+        ProbabilidadeDeSelecionarDaGeracaoFilhos = .90f,
+        CromossomaFactory = cromossomaFactory,
+        OutputService = outputService
+    };
+
+
+    return new AGHorarioEscolar<HorarioCromossoma>
+    {
+        Configuracao = agConfig,
+    };
+
+}
+
+void CarregarDadosCSV()
+{
+    try
+    {
+        AnsiConsole.Status()
+        .Spinner(Spinner.Known.Dots)
+        .Start("Obter estrutura dos horários...", ctx =>
+        {
+            ctx.Status("A carregar horário...");
+            horarioService.CarregarDados();
+            ctx.Status("A carregar disciplinas...");
+            disciplinaService.CarregarDados();
+            ctx.Status("A carregar professores...");
+            professoresService.CarregarDados();
+            ctx.Status("A carregar turmas...");
+            turmaService.CarregarDados();
+        });
+    }
+    catch (Exception ex)
+    {
+        AnsiConsole.Write("[red]Erro Fatal[/]");
+        AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+        return;
+    }
+}
